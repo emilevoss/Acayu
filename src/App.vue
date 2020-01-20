57557093c8a4f59b0ba23724b5d5555b2d36a78a
emile <emilevoss@gmail.com>
2020-01-20 14:22:40 +0200

    20-01-20

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,43 @@
 <template>
-  <div id="app">
-    <div id="nav">
+    <div class="clamp-1280" id="contentKids">
+
+      <!----- Header Section ----->
+      <section class="headerSection">
+      <div class="clamp-1280">
+          <img src="" alt="" class="logo">
+          Logo
+          <ul class="menu">
+            <li><a href="#about">About Me</a></li>
+            <li><a href="#services">Services</a></li>
+            <li><a href="#workshops">Workshops</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li><a href="#rates">Rates & Bookings</a></li>
+          </ul>
+      </div>
+      </section>
+
+      <!----- About Section ----->
+
+      <section class="aboutSection" id="about">
+        <div class="clamp-1280">About Sections goes here
+          <div class="title"></div>
+          <div class="aboutImg">
+            <img src="" alt="About IMG goes here">
+          </div>
+          <div class="aboutText">About Text goes here</div>
+        </div>
+      </section>
+
+
+
+
     </div>
-    <router-view/>
-  </div>
 </template>
 
 <style lang="less">
+  @import "css/global.less";
+  @import url('https://fonts.googleapis.com/css?family=Comfortaa:400,700&display=swap');
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
