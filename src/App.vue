c0d06164341a188f0b7e2c3fbc2f139cdf53bd42
emile <emilevoss@gmail.com>
2020-01-24 14:53:36 +0200

    23-01-20

--- src/App.vue
+++ src/App.vue
@@ -12,16 +12,14 @@
                     <li><a href="#about">About Me</a></li>
                     <li><a href="#services">Services</a></li>
                     <li><a href="#workshops">Workshops</a></li>
-                    <li><a href="#contact">Contact</a></li>
                     <li><a href="#rates">Rates & Bookings</a></li>
+                    <li><a href="#contact">Contact</a></li>
                 </ul>
               </div>
       </div>
       <div class="banner">
           <div class="banner-text">
-              <h2>
-                  <p>Header is going here</p>
-              </h2>
+              Every accomplishment starts with the decision to try
           </div>
       </div>
 
@@ -29,12 +27,12 @@
 <!----- About Section START ----->
 
       <section class="about-section clamp-1280" id="about">
-          <div class="title">About Me</div>
+          <h1>About Me</h1>
           <div class="flex-about">
             <div class="about-img-container">
                 <img src="@/assets/SmallLogo.jpg" class="about-img" alt="About IMG goes here">
             </div>
-            <div class="about-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam commodi consequuntur eius esse, est fugit hic inventore, laboriosam minima natus nemo nostrum placeat praesentium quas quibusdam quo reiciendis saepe sint tempore veritatis. A accusamus, accusantium atque culpa debitis dicta doloribus dolorum eius enim expedita fuga illum labore laborum magnam minus modi quae quam qui rem repudiandae saepe totam ut vitae!</div>
+            <h3>My name is Candice Voss, I have a psychology background and have worked as an au pair, facilitator at mainstream schools and as a therapist at a specialised therapy intervention centre for children with social, emotional, communication challenges and behavioural difficulties. For the past several years I worked at an NPO focusing on Autism, family support, parent and professional workshops, behaviour management, school support and training as well as support at Tygerberg Hospital and Red Cross Children’s Hospital. Content-Kids is my private practice where I focus on parenting support, inclusive education and teacher support as well as special needs support with a specific focus on autism, behavior management and alternative communication systems.</h3>
           </div>
 
       </section>
@@ -42,54 +40,149 @@
 <!---------------------------------------------------------------------------------->
 <!----- Collaborations Section START ----->
 
-      <section class="clamp-1280 collaborations-section" id="collaboration">
-        <div class="flex-colab">
-          <div class="tile">Collabs</div>
-          <h2>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi consequuntur dignissimos dolores ex!
-          </h2>
+    <div class="collaborations-section">
+      <section class="clamp-1280" id="collaboration">
+          <h1>Collabs</h1>
+          <div class="collab-info">
+              <h2>I work at Cape Town Therapy Hub on a Tuesday afternoons and consult closely with Naomi Botha from Speak Autism</h2>
+          </div>
+        <div class="flex-collab">
+            <div>
+                <h3>Cape Town Therapy Hub</h3>
+                <p>Cape Town Therapy Hub is a therapeutic center located in Green Point. The Center offers psychological services, speech therapists, occupational therapists, dietetics, diagnostic educational assessments, parent support as well as autism support. Therapists are all part of a multidisciplinary team who have a wealth of knowledge and experience working with families and children. The vision of Cape Town Therapy Hub is to offer professional and personalised therapeutic services to clients in a multidisciplinary team environment.</p>
+            </div>
+            <div>
+                <h3>Speak Autism!</h3>
+                <p>Naomi uses a wide range of tried and tested key support strategies and teaching tools, for supporting and meeting the individual needs of our children with Asperger which falls on the Autism spectrum. She uses developmental and behavioural approaches to focus on the individual differences of each child; providing relationship based interventions with the goals of releasing potential, increasing understanding, developing confidence, improving skills and changing behaviour</p>
+            </div>
         </div>
       </section>
+    </div>
 
 <!---------------------------------------------------------------------------------->
 <!----- Services Section START ----->
 
-      <section class="services-section clamp-1280" id="services">
-        <div class="container-services">
-
-            <div class="container-1">
-              <img src="" alt="1" class="servicesImg">
-              <h1>Lorem ipsum dolor sit amet.</h1>
-                <h2>Lorem ipsum dolor.</h2>
+      <section class="services-section" id="services">
+          <h1>Services</h1>
+        <div class="services-container">
+            <div class="services-offering">
+                <div class="servicesImg">
+              <img src="@/assets/SmallLogo.jpg" class="services-image" alt="">
+                </div>
+              <h3>Parenting Support</h3>
+                <p>I am passionate about parenting and am a mother of 2 amazing children. I have focused on behaviour management for many years, if you can get your child’s behavioural challenges under control parenting becomes a lot more enjoyable.<br><br>
+                    This can be achieved through understanding your child, positive parenting, putting up boundaries as a parent and creating routines in the home that will help your child feel secure and able to grow in independence. Parenting is hard work but can be a lot of fun too.<br><br>
+                    What to expect as a first time parent<br>
+                    Baby sign language<br>
+                    Playing with your child<br>
+                    Toilet training<br>
+                    Specific childhood difficulties (anxiety, ADHD, emotional regulation, helping your child to listen, house rules, childrens chores)<br><br>
+                    Positive parenting<br><br>
+                    Behavior management</p>
             </div>
-
-            <div class="container-2">
-              <img src="" alt="2" class="servicesImg">
-              <h1>Lorem ipsum dolor sit amet.</h1>
-                <h2>Lorem ipsum dolor.</h2>
+            <div class="services-offering">
+                <div class="servicesImg">
+                    <img src="@/assets/SmallLogo.jpg" class="services-image" alt="">
+                </div>
+                <h3>School Support</h3>
+                    <p>I have a special interest in inclusive education and feel strongly that all children have the right to an education. Children all learn differently and at different paces and there are many strategies that can be implemented to embrace this.
+                        This means equipping and supporting schools, teachers, teacher assistants and facilitators to use inclusive teaching tools and methods. <br><br>It is hard to contain my excitement for inclusive education and what teachers could be doing in their classes. As well as educate and assist teachers with their IEP needs.
+                        Classroom strategies for the struggling child
+                        Inclusive strategies
+                        IEP assistance
+                        Facilitation placement and training</p>
             </div>
-
-            <div class="container-3">
-              <img src="" alt="3" class="servicesImg">
-              <h1>Lorem ipsum dolor sit amet.</h1>
-                <h2>Lorem ipsum dolor.</h2>
+            <div class="services-offering">
+                <div class="servicesImg">
+                    <img src="@/assets/SmallLogo.jpg" class="services-image" alt="">
+                </div>
+                <h3>Special Needs Support</h3>
+                    <p>I have had international and national experience with Autism as well as Down Syndrome. I have been privileged enough to have been able to dedicate a few years to truly trying to understand Autism and Down Syndrome. Parenting is overwhelming, parenting a special needs child means that you really need so much more support than anyone can comprehend.
+                        There is often a team of people supporting each special needs child, but there is not a support system for the parents of those children.<br><br> Special needs children often have many different therapists that they go to, but not many people spend time explaining those therapies to the parents. I hope to bridge this gap between therapists and parents as well as support parents in their journey with their special needs children, especially those that have children with communication barriers.<br><br>
+                        Autism<br>
+                        Down Syndrome<br>
+                        ADHD<br>
+                        Sensory Processing<br>
+                        Alternative communication strategies<br>
+                        Behavior management<br>
+                        Early development<br>
+                        Au pair training and support
+                        School guidance</p>
             </div>
-
         </div>
       </section>
 
 <!---------------------------------------------------------------------------------->
 <!----- Workshops Section START ----->
 
-      <section class="clamp-1280 workshop-section" id="workshops">
-
-
-
-
-      </section>
+       <section class="clamp-1280 workshop-section" id="workshops">
+              <h1>Workshops</h1>
+              <div class="workshop-container">
+                  <div class="workshop-offering">
+                      <h3>Parent Workshops</h3>
+                      <p>Positive Parenting<br>
+                          Behaviour Management<br>
+                          Toilet Training
+                      </p>
+                  </div>
+                  <div class="workshop-offering">
+                      <h3>Teacher Workshops</h3>
+                      <p>Behaviour management in the classroom
+                          Inclusive Strategies for the classroom
+                          What is Autism
+                      </p>
+                  </div>
+                  <div class="workshop-offering">
+                      <h3>Student Talks</h3>
+                      <p>What is Autism (Children 3-6 years old) <br><br>
+                          What is Autism (Children 6-14 years old)<br><br>
+                          What is Autism (Teenagers 15-18 years old)
+                      </p>
+                  </div>
+              </div>
+        </section>
 
 <!---------------------------------------------------------------------------------->
+<!----- Rates & Bookings Section START ----->
 
+
+       <section class="clamp-1280 workshop-section" id="rates">
+            <h1>Rates & Bookings</h1>
+            <div class="workshop-container">
+                <div class="workshop-offering">
+                    <h3>Therapy sessions</h3>
+                    <p>Therapy sessions are an hour long and can take place at my offices in Bergvliet or Sea Point.<br><br>
+                        R500 per hour and R250 per half hour thereafter
+                    </p>
+                </div>
+                <div class="workshop-offering">
+                    <h3>Home visits</h3>
+                    <p>R500 per hour and R250 per half hour thereafter<br><br>
+                        Billed hours include travel time<br>
+                        AA rates may apply
+                    </p>
+                </div>
+                <div class="workshop-offering">
+                    <h3>Workshops</h3>
+                    <p>Workshops costs vary depending on the size and length of workshop.<br><br>
+                        Parent workshops are roughly R250 per family<br><br>
+                        Please contact me for teacher workshops and student talks.
+                    </p>
+                </div>
+            </div>
+        </section>
+
+<!---------------------------------------------------------------------------------->
+        <!----- Contact Section START ----->
+
+        <section class="clamp-1280" id="contact">
+            <h1>Contact</h1>
+            <div class="collab-info">
+                <h2>If you have any quiries please contact me</h2>
+            </div>
+        </section>
+
+<!---------------------------------------------------------------------------------->
 
 
     </div>
