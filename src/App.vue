1ab3ed86334093da6eb67c561e63ad7b7b15e6c4
emile <emilevoss@gmail.com>
2020-02-07 15:05:37 +0200

    070220

--- src/App.vue
+++ src/App.vue
@@ -220,7 +220,7 @@
             </div>
             <div class="contact-wrap">
                 <div class="contact-cell">
-                    <img src="@/src/assets/phone-portrait-outline.svg" alt="">
+                    <img src="" alt="">
                     <h3>0823791701</h3>
                 </div>
                 <div class="contact-email">
