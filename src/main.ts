0a5c1007f2f45aa1df1ce2a997d8fd59f474f1f8
emile <emilevoss@gmail.com>
2020-02-10 11:52:53 +0200

    100220

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
-import EvaIcons from 'vue-eva-icons'
+import Icon from 'vue-awesome/components/Icon'
+
+
+Vue.component('v-icon', Icon)
+
 
 Vue.config.productionTip = false
 
