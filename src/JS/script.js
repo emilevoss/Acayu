5b9bf3ca06a102a7957b1c631bf6e39407475e13
emile <emilevoss@gmail.com>
2020-02-18 12:47:01 +0200

    140220

--- src/JS/script.js
+++ src/JS/script.js
@@ -1,1 +1,15 @@
 /* Scroll to sections from menu */
+
+const sectionOne = document.querySelector('.header-section');
+
+const options = {};
+
+const observer = new IntersectionObserver(function (entries, observer) {
+
+    entries.forEach(entry => {
+        console.log(entry);
+    });
+
+}, options);
+
+observer.observe(sectionOne);
