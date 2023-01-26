5b324d15b37687da09477b62abfe76e779d38720
Emile Voss <emilevoss@gmail.com>
2023-01-26 17:39:23 +0200

    remove dist

--- src/App.vue
+++ src/App.vue
@@ -359,7 +359,6 @@
 <script lang="ts">
 
     import {Component, Vue} from 'vue-property-decorator';
-    import axios from 'axios';
     import VueSlickCarousel from 'vue-slick-carousel';
     import 'vue-slick-carousel/dist/vue-slick-carousel.css';
     import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css';
